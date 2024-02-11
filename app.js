63d326f52d891e98b7e9e6a1645021cbd35b40b0
LANALANA1111 <lanagelashvili1111@gmail.com>
2024-02-12 00:58:02 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function enterClick(event) {
+  if (event.key === "Enter") {
+    if (inputBox.value === "") {
+      alert("enter something");
+    } else {
+      let li = document.createElement("li");
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+      let span = document.createElement("span");
+      span.innerHTML = "\u00d7";
+      li.appendChild(span);
+    }
+    inputBox.value = "";
+    // saveData();
+  }
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      //   saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      //   saveData();
+    }
+  },
+  false
+);
+
+// function saveData() {
+//   localStorage.setItem("Data", listContainer.innerHTML);
+// }
+
+// function showTask(){
+//     listContainer.innerHTML = localStorage.getItem("data")
+// }
+// showTask()
